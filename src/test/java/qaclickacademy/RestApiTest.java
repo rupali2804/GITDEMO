c41990081814f9da0dc26392dbbfc57c3398a8c3
rupali <rupali072@gmail.com>
2019-04-01 23:08:17 +0530

    Commiting maven project

---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void PostJira()
+	{
+		System.out.println("Post Jira");
+	}
+@Test
+public void Twitter() {
+	System.out.println("Twitter");
+}
+}
